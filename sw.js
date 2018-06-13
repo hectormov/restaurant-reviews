51bf8da897d8c9f01d442cff93406898ffc51721
Hector Moreno <hmoreno@pros.com>
2018-06-13 15:04:49 -0500

    Feat: Service Worker Added a second approach of adding to the cache to test on github

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-let staticCacheName = 'restaurantsV1';
+let staticCacheName = 'restaurantsV2';
 
 //Creates the cache with all the required urls
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function(cache) {
-            cache.addAll([
-                //URLS TO ADD
-                '/',
-                '/index.html',
-                '/restaurant.html'
-                // '/css/styles.css',
-                // '/js/dbhelper.js',
-                // '/js/main.js',
-                // '/js/restaurant_info.js',
-            ]);
-        }).catch(function (error) {
-            console.log('Error creating cache', error);
-        })
-    );
+    // event.waitUntil(
+        // caches.open(staticCacheName).then(function(cache) {
+        //     cache.addAll([
+        //         //URLS TO ADD
+        //         '/',
+        //         '/index.html',
+        //         '/restaurant.html'
+        //         // '/css/styles.css',
+        //         // '/js/dbhelper.js',
+        //         // '/js/main.js',
+        //         // '/js/restaurant_info.js',
+        //     ]);
+        // }).catch(function (error) {
+        //     console.log('Error creating cache', error);
+        // })
+    // );
 });
 
 //Deletes the old cache in case there is a new version
@@ -37,31 +37,51 @@ self.addEventListener('activate', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    //Save all pictures to Cache
-    let url = event.request.url;
-    if(url.includes('jpg') || url.includes('.js') || url.includes('.css') || url.includes('.png')) {
-        caches.open(staticCacheName).then(function(cache) {
-            cache.add(url).catch(function (error) {
-                console.log('saving cache', error);
-            });
-        });
-    }
+///Approach 1, chosing which files to add to cache.
+// self.addEventListener('fetch', function(event) {
+//     //Save all pictures to Cache
+//     let url = event.request.url;
+//     if(url.includes('jpg') || url.includes('.js') || url.includes('.css') || url.includes('.png')) {
+//         caches.open(staticCacheName).then(function(cache) {
+//             cache.add(url).catch(function (error) {
+//                 console.log('saving cache', error);
+//             });
+//         });
+//     }
 
-    //If it exists on cache load from there first..
+//     //If it exists on cache load from there first..
+//     event.respondWith(
+//         caches.match(event.request).then(function(response) {
+//             if (response) {
+//                 return response;
+//             }
+//             return fetch(event.request).catch(function(error){
+//                 console.log('failed to fetch when not in cache', error)
+//             });
+//             // .catch(function(error) {
+//             //     console.log('Failed while fetching data', error);
+//             // });
+//         }).catch(function(error) {
+//             console.log('Error cache response', error);
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(function(response){
             if (response) {
                 return response;
+            } else {
+                return fetch(event.request).then(function(fetchedResponse) {
+                    return caches.open(staticCacheName).then(function(cache) {
+                        cache.add(event.request.url);
+                        return fetchedResponse;
+                    })
+                }).catch(function(error) {
+                    console.log('Failed to Load', error);
+                });
             }
-            return fetch(event.request).catch(function(error){
-                console.log('failed to fetch when not in cache', error)
-            });
-            // .catch(function(error) {
-            //     console.log('Failed while fetching data', error);
-            // });
-        }).catch(function(error) {
-            console.log('Error cache response', error);
         })
     );
 });
