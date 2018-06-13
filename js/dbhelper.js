3484ee3bcab59be80a73128d49e62a7fa1c3e704
Hector Moreno <hectormov@gmail.com>
2018-06-12 20:17:44 -0500

    BUG: Service Worker Added loaction.path to imageUrlForRestaurant function

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (loaction.pathname + `img/${restaurant.photograph}`);
   }
 
   /**
